488f9de103c3fddc1e096a07f50d7f28e7f5051f
Sum Dood <sumdood31@gmail.com>
2013-10-11 12:37:04 -0500

    start of base mvc project
    asdf

---
+++ MVC4Template/MVC4Template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC4Template.MvcApplication" Language="C#" %>
---
+++ MVC4Template/MVC4Template/Models/PageBaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC3Template.Models
+{
+    public class PageBaseModel
+    {
+        //SEO DATA
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Keywords { get; set; }
+
+        //PAGE VARS, MOSTLY USED FOR JAVASCRIPT
+        public string ActiveTopNavLink { get; set; }
+    }
+}
